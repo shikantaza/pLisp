3c0cdda7e5586a1156ae3f80ea91bcab00c2dbc3
Rajesh Jayaprakash <rajesh.jayaprakash@gmail.com>
2013-04-03 13:21:28 +0530

    v0.2

---
+++ math.lisp
@@ -0,0 +1,17 @@
+(create-package "math")
+
+(in-package "math")
+
+(defun abs (x)
+  (if (>= x 0)
+      x
+    (* -1 x)))
+
+(defun power (a b)
+  (if (eq b 0)
+        1
+    (if (< b 0)
+        (/ 1.0 (power a (abs b)))
+      (* a (power a (- b 1))))))
+
+(in-package "user")
