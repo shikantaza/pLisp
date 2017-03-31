b514ce600dc13f4be5bf9552437ff4f9ca0deddd
Rajesh Jayaprakash <rajesh.jayaprakash@gmail.com>
2017-03-31 12:22:48 +0530

    Added getline() to main.c (needed for win32 pipe mode)

--- src/main.c
+++ src/main.c
@@ -3246,6 +3246,61 @@ OBJECT_PTR rewrite_symbols(OBJECT_PTR exp)
     return map(rewrite_symbols, exp);
 }
 
+#ifdef WIN32
+//from http://stackoverflow.com/questions/735126/are-there-alternate-implementations-of-gnu-getline-interface/735472#735472
+size_t getline(char **lineptr, size_t *n, FILE *stream) {
+    char *bufptr = NULL;
+    char *p = bufptr;
+    size_t size;
+    int c;
+
+    if (lineptr == NULL) {
+    	return -1;
+    }
+    if (stream == NULL) {
+    	return -1;
+    }
+    if (n == NULL) {
+    	return -1;
+    }
+    bufptr = *lineptr;
+    size = *n;
+
+    c = fgetc(stream);
+    if (c == EOF) {
+    	return -1;
+    }
+    if (bufptr == NULL) {
+    	bufptr = GC_MALLOC(128);
+    	if (bufptr == NULL) {
+    		return -1;
+    	}
+    	size = 128;
+    }
+    p = bufptr;
+    while(c != EOF) {
+    	if ((p - bufptr) > (size - 1)) {
+    		size = size + 128;
+    		bufptr = GC_REALLOC(bufptr, size);
+    		if (bufptr == NULL) {
+    			return -1;
+    		}
+    	}
+    	*p++ = c;
+    	if (c == '\n') {
+    		break;
+    	}
+    	c = fgetc(stream);
+    }
+
+    *p++ = '\0';
+    *lineptr = bufptr;
+    *n = size;
+
+    return p - bufptr - 1;
+}
+#endif
+
 int main(int argc, char **argv)
 {
   int opt, i;
