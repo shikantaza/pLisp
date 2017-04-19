b7078abb7bb6701a379480b50fbc7979f747cb03
Rajesh Jayaprakash <rajesh.jayaprakash@gmail.com>
2017-04-19 15:46:31 +0530

    1. Script to generate .deb for Linux 2. Downloads directory added 3. .deb file for i386 generated

---
+++ scripts/build_deb.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+pkg=plisp_0.1-1_i386
+
+mkdir $pkg
+
+mkdir -p $pkg/usr/local/bin
+mkdir -p $pkg/usr/local/lib
+mkdir -p $pkg/usr/local/share/plisp/icons
+mkdir -p $pkg/usr/local/share/doc/plisp
+
+cp ../plisp $pkg/usr/local/bin
+
+cp ../.libs/libplisp.a $pkg/usr/local/lib
+cp ../libplisp.la $pkg/usr/local/lib
+cp ../.libs/libplisp.so $pkg/usr/local/lib
+
+cp ../data/help.json $pkg/usr/local/share/plisp
+cp ../lib/plisp_full_monty_compiler.lisp $pkg/usr/local/share/plisp
+cp ../data/plisp.lang $pkg/usr/local/share/plisp
+cp ../icons/* $pkg/usr/local/share/plisp/icons
+
+cp ../doc/help.html $pkg/usr/local/share/doc/plisp
+cp ../doc/pLisp_User_Manual.pdf $pkg/usr/local/share/doc/plisp
+cp ../doc/tutorial.lisp $pkg/usr/local/share/doc/plisp
+
+mkdir -p $pkg/DEBIAN
+cp control $pkg/DEBIAN
+
+dpkg-deb --build $pkg
+
+mv $pkg.deb ..
+
+rm -rf $pkg
