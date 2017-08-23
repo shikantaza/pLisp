a4a26a26443afd0f5aa97f273cd4398a67255d7b
Rajesh Jayaprakash <rajesh.jayaprakash@gmail.com>
2017-08-23 13:05:59 +0530

    Replaced pos.lisp code with simpler and cleaner version

--- lib/pos.lisp
+++ lib/pos.lisp
@@ -15,120 +15,41 @@
 ;;  You should have received a copy of the GNU General Public License
 ;;  along with pLisp.  If not, see <http://www.gnu.org/licenses/>.
 
-(defmacro create-instance (superclass-name
-                           instance-var-list
-                           instance-init-form
-                           instance-methods)
-  (let ((sym (gensym)))
-    (let ((e1 (let ((lst nil))
-                (dolist (var instance-var-list)
-                  (set lst (append lst (list (list (car var) (cadr var))))))
-                lst))
-          (e2 (list 'parent (if (null superclass-name) 
-                                nil 
-                              (list 'call-method superclass-name ''make-instance))))
-          (e3 (list 'get-parent (list 'lambda nil 'parent)))
-          (e4 (list 'initialize instance-init-form))
-          (e5 (let ((lst nil))
-                (dolist (var instance-methods)
-                  (set lst (append lst (list (list (car var)
-                                                   (list 'lambda 
-                                                         (cadr var) 
-                                                         (caddr var)))))))
-                lst))
-          (e6 (let ((lst nil))
-                (dolist (var instance-var-list)
-                  (progn
-                    (set lst (append lst (list (list (caddr var)
-                                                     (list 'lambda nil (car var))))))
-                    (set lst (append lst (list (list (cadddr var)
-                                                     (list 'lambda
-                                                           (list sym)
-                                                           (list 'set (car var) sym))))))))
-                lst)))
-      (let ((e7 (append e1 (list e2)))
-            (e8 (map (lambda (x) (list 'cons
-                                       (list quote (car x))
-                                       (car x)))
-                     e5))
-            (e9 (map (lambda (x) (list 'cons
-                                       (list quote (car x))
-                                       (car x)))
-                     e6)))      
-        `(let ,e7
-           (let (,e3 ,e4 ,@e5 ,@e6)
-             ,(if (not (null e4) '(progn (if (not (null parent)) (call-method parent 'initialize)) (initialize))))
-             (list (cons 'get-parent get-parent)
-                   (cons 'initialize initialize)
-                   ,@e8
-                   ,@e9)))))))
+(defmacro build-getter (sym)
+  `(symbol (concat-strings "get-" (symbol-name ,sym))))
 
-(defmacro define-class (class-name
-                        superclass-name
-                        class-var-list
-                        instance-var-list
-                        class-init-form
-                        instance-init-form
-                        class-methods
-                        instance-methods)
-  (let ((sym (gensym)))
-    (let ((e1 (let ((lst nil))
-                (dolist (var class-var-list)
-                  (set lst (append lst (list (list (car var) (cadr var))))))
-                lst))
-          (e2 (list 'get-superclass (list 'lambda nil superclass-name)))
-          (e3 (list 'initialize class-init-form))
-          (e4 (let ((lst nil))
-                (dolist (var class-methods)
-                  (set lst (append lst (list (list (car var)
-                                                   (list 'lambda 
-                                                         (cadr var) 
-                                                         (caddr var)))))))
-                lst))
-          (e5 (let ((lst nil))
-                (dolist (var class-var-list)
-                  (progn
-                    (set lst (append lst (list (list (caddr var)
-                                                     (list 'lambda nil (car var))))))
-                    (set lst (append lst (list (list (cadddr var)
-                                                     (list 'lambda
-                                                           (list sym)
-                                                           (list 'set (car var) sym))))))))
-                lst)))
-      (let ((e6 (map (lambda (x) (list 'cons
-                                       (list quote (car x))
-                                       (car x)))
-                     e4))
-            (e7 (map (lambda (x) (list 'cons
-                                       (list quote (car x))
-                                       (car x)))
-                     e5)))
-        `(define ,class-name (let ,e1
-                               (let (,e2 ,e3 ,@e4 ,@e5 (make-instance (lambda nil (create-instance ,superclass-name 
-                                                                                                   ,instance-var-list 
-                                                                                                   ,instance-init-form 
-                                                                                                   ,instance-methods))))
-                                 ,(if (not (null e3) '(progn (if (not (null superclass-name)) (call-method superclass-name 'initialize)) (initialize))))
-                                 (list (cons 'get-superclass get-superclass)
-                                       (cons 'initialize initialize)
-                                       ,@e6
-                                       ,@e7
-                                       (cons 'make-instance make-instance)))))))))
+(defmacro build-setter (sym)
+  `(symbol (concat-strings "set-" (symbol-name ,sym))))
 
-(defun get-method (obj method-name)
-  (if (null obj)
-      nil
-    (progn (dolist (x obj)
-             (if (eq (car x) method-name)
-                 (return-from get-method (cdr x))))
-           (get-method (call-method obj 'get-parent) method-name))))
+(defun send-message (obj method-name &rest params)
+  (dolist (m obj)
+    (if (eq (car m) method-name)
+        (return-from send-message (apply (cdr m) params))))
+  (let ((parent (send-message obj 'get-parent)))
+    (if parent
+        (apply send-message (concat (list parent method-name) params))
+      (throw (exception 'method-does-not-exist (symbol-name method-name))))))
 
-(defun call-method (obj method-name &rest params)
-  (let ((method (get-method obj method-name)))
-    (if (null method)
-        (throw (exception 'method-does-not-exist (symbol-name method-name)))
-      (apply method params))))
+(defmacro create-object (super-class vars init-form methods)
+  (let ((e1 (map (lambda (x) (list x)) vars))
+        (e2 (map (lambda (x) (list 'cons (list 'quote (build-getter x)) (list 'lambda nil x))) vars))
+        (e3 (map (lambda (x) (list 'cons (list 'quote (build-setter x)) (list 'lambda '(v) (list 'set x 'v)))) vars))
+        (e4 (map (lambda (x) (list 'cons (list 'quote (first x)) (list 'lambda (second x) (third x)))) methods))
+        (e5 (gensym)))
+    `(let ,e1
+       (let ((,e5 ,(if (null super-class) nil (list 'send-message super-class ''create-instance))))
+         ,init-form
+       ,(concat (list 'list) e2 e3 e4 (list (list 'cons (list 'quote 'get-parent) (list 'lambda () e5))))))))
 
-(defmacro make-instance (class)
-  `(call-method ,class 'make-instance))
+(defmacro create-class (super-class class-vars inst-vars class-init-form inst-init-form class-methods inst-methods)
+  (let ((e1 (map (lambda (x) (list x)) class-vars))
+        (e2 (map (lambda (x) (list 'cons (list 'quote (build-getter x)) (list 'lambda nil x))) class-vars))
+        (e3 (map (lambda (x) (list 'cons (list 'quote (build-setter x)) (list 'lambda '(v) (list 'set x 'v)))) class-vars))
+        (e4 (list (list 'cons (list 'quote 'create-instance) (list 'lambda nil (list 'create-object super-class inst-vars inst-init-form inst-methods)))))
+        (e5 (map (lambda (x) (list 'cons (list 'quote (first x)) (list 'lambda (second x) (third x)))) class-methods)))
+    `(let ,e1
+       ,class-init-form
+       ,(concat (list 'list) e2 e3 e4 e5))))
 
+(defmacro create-instance (class)
+  `(send-message ,class 'create-instance))
