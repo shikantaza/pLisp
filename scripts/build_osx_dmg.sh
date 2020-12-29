e9a33ec0fa0b3f7bbffd3b76485bff1323ad2a30
Rajesh Jayaprakash <rajesh.jayaprakash@gmail.com>
2020-12-29 16:57:31 +0530

    Update OS X build script for port to LLVM 11.0.0

--- scripts/build_osx_dmg.sh
+++ scripts/build_osx_dmg.sh
@@ -10,7 +10,7 @@ CFLAGS="-I/usr/local/include -D__OSX_BUNDLE__"
 
 make distclean
 
-./configure LLVMDIR=/Users/a103035/clang+llvm-8.0.0-x86_64-apple-darwin/
+./configure LLVMDIR=/Users/a103035/clang+llvm-11.0.0-x86_64-apple-darwin/
 
 sudo make install
 
